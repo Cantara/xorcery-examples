c8a70f594535cb68d0c68517ed6f14fb6718c917
totto <totto@totto.org>
2025-10-08 15:33:53 +0200

    work to update to latest xorcery.

--- xorcery-examples-streaming/src/test/java/com/exoreaction/xorcery/examples/streaming/test/StreamingTest.java
+++ xorcery-examples-streaming/src/test/java/com/exoreaction/xorcery/examples/streaming/test/StreamingTest.java
@@ -1,6 +1,6 @@
 package com.exoreaction.xorcery.examples.streaming.test;
 
-import com.exoreaction.xorcery.runner.Main;
+import dev.xorcery.runner.Main;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
 
